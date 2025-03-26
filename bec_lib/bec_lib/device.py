bec-project/bec

feat(rpc): rpc objects should have the original class name

bec_lib/bec_lib/device.py
@@ -161,6 +161,7 @@ class DeviceBase:
         config: dict = None,
         parent=None,
         signal_info: dict = None,
+        class_name: str | None = None,
     ) -> None:
         """
         Args:
@@ -170,6 +171,7 @@ class DeviceBase:
             signal_info (dict, optional): The signal info dictionary. Defaults to None.
         """
         self.name = name
+        self._class_name = class_name or object.__getattribute__(self, "__class__").__name__
         self._signal_info = signal_info
         self._config = config
         if info is None:
@@ -481,8 +483,9 @@ class DeviceBase:
             else:
                 self._custom_rpc_methods[user_access_name] = DeviceBase(
                     name=user_access_name,
-                    info={"device_info": {"custom_user_access": descr}},
+                    info={"device_info": {"custom_user_access": descr["info"]}},
                     parent=self,
+                    class_name=descr["class_name"],
                 )
                 setattr(self, user_access_name, self._custom_rpc_methods[user_access_name])
 
@@ -652,6 +655,8 @@ class DeviceBase:
         # pylint: disable=import-outside-toplevel
         from bec_lib.devicemanager import DeviceManagerBase
 
+        class_name = obj._class_name
+
         if isinstance(obj.parent, DeviceManagerBase):
             # pylint: disable=protected-access
             config = "".join(
@@ -659,7 +664,7 @@ class DeviceBase:
             )
             separator = "--" * 10
             return (
-                f"{type(obj).__name__}(name={obj.name},"
+                f"{class_name}(name={obj.name},"
                 f" enabled={obj.enabled}):\n{separator}\nDetails:\n\tDescription:"
                 f" {obj._config.get('description', obj.name)}\n\tStatus:"
                 f" {'enabled' if obj.enabled else 'disabled'}\n\tRead only:"
@@ -670,7 +675,7 @@ class DeviceBase:
                 f" {obj._config.get('deviceTags', [])}\n\tUser parameter:"
                 f" {obj._config.get('userParameter')}\n{separator}\nConfig:\n{config}"
             )
-        return f"{type(obj).__name__}(name={obj.name}, enabled={obj.enabled})"
+        return f"{class_name}(name={obj.name}, enabled={obj.enabled})"
 
 
 class OphydInterfaceBase(DeviceBase):

bec_server/bec_server/device_server/devices/device_serializer.py
@@ -52,7 +52,10 @@ def get_custom_user_access_info(obj: Any, obj_interface: dict) -> dict:
                 if is_serializable(obj_member):
                     obj_interface[var] = {"type": type(obj_member).__name__}
                 elif get_device_base_class(obj_member) == "unknown":
-                    obj_interface[var] = get_custom_user_access_info(obj_member, {})
+                    obj_interface[var] = {
+                        "info": get_custom_user_access_info(obj_member, {}),
+                        "class_name": obj_member.__class__.__name__,
+                    }
                 else:
                     continue
             else:
@@ -172,6 +175,7 @@ def get_device_info(
             "device_attr_name": getattr(obj, "attr_name", ""),
             "device_dotted_name": getattr(obj, "dotted_name", ""),
             "device_base_class": get_device_base_class(obj),
+            "device_class": obj.__class__.__name__,
             "read_access": getattr(obj, "read_access", None),
             "write_access": getattr(obj, "write_access", None),
             "signals": signals,

d-perl/bec_ci_test

bec_lib/bec_lib/device.py
@@ -161,6 +161,7 @@ class DeviceBase:
         config: dict = None,
         parent=None,
         signal_info: dict = None,
+        class_name: str | None = None,
     ) -> None:
         """
         Args:
@@ -170,6 +171,7 @@ class DeviceBase:
             signal_info (dict, optional): The signal info dictionary. Defaults to None.
         """
         self.name = name
+        self._class_name = class_name or object.__getattribute__(self, "__class__").__name__
         self._signal_info = signal_info
         self._config = config
         if info is None:
@@ -481,8 +483,9 @@ class DeviceBase:
             else:
                 self._custom_rpc_methods[user_access_name] = DeviceBase(
                     name=user_access_name,
-                    info={"device_info": {"custom_user_access": descr}},
+                    info={"device_info": {"custom_user_access": descr["info"]}},
                     parent=self,
+                    class_name=descr["device_class"],
                 )
                 setattr(self, user_access_name, self._custom_rpc_methods[user_access_name])
 
@@ -652,6 +655,8 @@ class DeviceBase:
         # pylint: disable=import-outside-toplevel
         from bec_lib.devicemanager import DeviceManagerBase
 
+        class_name = obj._class_name
+
         if isinstance(obj.parent, DeviceManagerBase):
             # pylint: disable=protected-access
             config = "".join(
@@ -659,7 +664,7 @@ class DeviceBase:
             )
             separator = "--" * 10
             return (
-                f"{type(obj).__name__}(name={obj.name},"
+                f"{class_name}(name={obj.name},"
                 f" enabled={obj.enabled}):\n{separator}\nDetails:\n\tDescription:"
                 f" {obj._config.get('description', obj.name)}\n\tStatus:"
                 f" {'enabled' if obj.enabled else 'disabled'}\n\tRead only:"
@@ -670,7 +675,7 @@ class DeviceBase:
                 f" {obj._config.get('deviceTags', [])}\n\tUser parameter:"
                 f" {obj._config.get('userParameter')}\n{separator}\nConfig:\n{config}"
             )
-        return f"{type(obj).__name__}(name={obj.name}, enabled={obj.enabled})"
+        return f"{class_name}(name={obj.name}, enabled={obj.enabled})"
 
 
 class OphydInterfaceBase(DeviceBase):

bec_server/bec_server/device_server/devices/device_serializer.py
@@ -52,7 +52,10 @@ def get_custom_user_access_info(obj: Any, obj_interface: dict) -> dict:
                 if is_serializable(obj_member):
                     obj_interface[var] = {"type": type(obj_member).__name__}
                 elif get_device_base_class(obj_member) == "unknown":
-                    obj_interface[var] = get_custom_user_access_info(obj_member, {})
+                    obj_interface[var] = {
+                        "info": get_custom_user_access_info(obj_member, {}),
+                        "device_class": obj_member.__class__.__name__,
+                    }
                 else:
                     continue
             else:
@@ -172,6 +175,7 @@ def get_device_info(
             "device_attr_name": getattr(obj, "attr_name", ""),
             "device_dotted_name": getattr(obj, "dotted_name", ""),
             "device_base_class": get_device_base_class(obj),
+            "device_class": obj.__class__.__name__,
             "read_access": getattr(obj, "read_access", None),
             "write_access": getattr(obj, "write_access", None),
             "signals": signals,