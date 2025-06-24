fix(live_table): handle precision better

bec_ipython_client/bec_ipython_client/callbacks/live_table.py
@@ -2,7 +2,9 @@ from __future__ import annotations
 
 import time
 from collections.abc import Callable
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any, SupportsFloat
+
+import numpy as np
 
 from bec_ipython_client.prettytable import PrettyTable
 from bec_ipython_client.progressbar import ScanProgressBar
@@ -266,39 +268,36 @@ class LiveUpdatesTable(LiveUpdatesBase):
 
         if self.point_id % 100 == 0:
             print(self.table.get_header_lines())
-        ind = 0
+
+        signals_precisions = []
         for dev in self.devices:
             if dev in self.bec.device_manager.devices:
                 obj = self.bec.device_manager.devices[dev]
                 for hint in obj._hints:
                     signal = self.point_data.content["data"].get(obj.root.name, {}).get(hint)
-                    if signal is None or signal.get("value") is None:
-                        print_value = "N/A"
+                    if signal is None:
+                        signals_precisions.append((None, None))
                     else:
-                        try:
-                            precision = getattr(obj, "precision")
-                        except AttributeError:
-                            precision = 2
-                        value = signal.get("value")
-                        if isinstance(value, (int, float)):
-                            print_value = f"{value:.{precision}f}"
-                        else:
-                            print_value = str(value)
-                    self.dev_values[ind] = print_value
-                    ind += 1
+                        prec = getattr(obj, "precision", 2)
+                        if not isinstance(prec, int) or prec < 1:
+                            logger.warning(f"Device {dev} reported malformed precision of {prec}!")
+                            prec = 2
+                        signals_precisions.append((signal, prec))
             else:
-                signal = self.point_data.content["data"].get(dev, {})
-                value = signal.get("value")
-                if value is not None:
-                    if isinstance(value, (int, float)):
-                        print_value = f"{value:.2f}"
-                    else:
-                        print_value = str(value)
-                else:
-                    print_value = "N/A"
-                self.dev_values[ind] = print_value
-                ind += 1
+                signals_precisions.append((self.point_data.content["data"].get(dev, {}), 2))
+
+        for i, (signal, precision) in enumerate(signals_precisions):
+            self.dev_values[i] = self._format_value(signal, precision)
+
         print(self.table.get_row(str(self.point_id), *self.dev_values))
+
+    def _format_value(self, signal: dict | None, precision: int = 2):
+        if signal is None:
+            return "N/A"
+        val = signal.get("value")
+        if isinstance(val, SupportsFloat) and not isinstance(val, np.ndarray):
+            return f"{float(val):.{precision}f}"
+        return str(val)
 
     def close_table(self):
         """close the table and print the footer"""

bec_ipython_client/tests/client_tests/test_live_table.py
@@ -215,7 +215,10 @@ def test_print_table_data_hinted_value(client_with_grid_scan):
         assert mock_client_msgs.called
 
 
-def test_print_table_data_hinted_value_with_precision(client_with_grid_scan):
+@pytest.mark.parametrize(
+    ["prec", "expected_prec"], [(2, 2), (3, 3), (4, 4), (-1, 2), ("precision", 2)]
+)
+def test_print_table_data_hinted_value_with_precision(client_with_grid_scan, prec, expected_prec):
     client, request_msg = client_with_grid_scan
     response_msg = messages.RequestResponseMessage(
         accepted=True, message={"msg": ""}, metadata={"RID": "something"}
@@ -224,7 +227,7 @@ def test_print_table_data_hinted_value_with_precision(client_with_grid_scan):
     client.queue.request_storage.update_with_response(response_msg)
     live_update = LiveUpdatesTable(client, {"scan_progress": 10}, request_msg)
     client.device_manager.devices["samx"]._info["hints"] = {"fields": ["samx_hint"]}
-    client.device_manager.devices["samx"].precision = 2
+    client.device_manager.devices["samx"].precision = prec
     live_update.point_data = messages.ScanMessage(
         point_id=0,
         scan_id="",
@@ -239,12 +242,14 @@ def test_print_table_data_hinted_value_with_precision(client_with_grid_scan):
     ):
         live_update.dev_values = (len(live_update._get_header()) - 1) * [0]
         live_update.print_table_data()
-        mocked_table.get_row.assert_called_with("0", f"{0:.2f}")
+        mocked_table.get_row.assert_called_with("0", f"{0:.{expected_prec}f}")
 
 
 @pytest.mark.parametrize(
     "value,expected",
     [
+        (np.int32(1), "1.00"),
+        (np.float64(1.00000), "1.00"),
         (0, "0.00"),
         (1, "1.00"),
         (0.000, "0.00"),